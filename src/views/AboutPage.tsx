f19895b33693fadd7350d4ea0ae295c9f62e6f90
nebosite <ericjorg@thejcrew.net>
2019-12-09 09:58:20 -0800

    A little more cleanup

--- src/views/AboutPage.tsx
+++ src/views/AboutPage.tsx
@@ -1,5 +1,4 @@
 import * as React from "react";
-
 import { observer, inject } from "mobx-react";
 import { IAppModel } from "models/i_appmodel";
 
@@ -9,14 +8,11 @@ export default class AboutPage
   extends React.Component<{appModel?: IAppModel}> {
 
   render() {
-    const { appModel } = this.props;
-
     return (
-      <div className="about">
-        <h2 className="page-title">About</h2>
-        <div className="alert alert-primary query-row">
-          <span>Combobox selected Item: </span>
-          <b className="attention-msg">{appModel.selectedItem}</b>
+      <div>
+        <h2>About</h2>
+        <div>
+          Combobox selected Item: <b>{this.props.appModel.selectedItem}</b>
         </div>
       </div>
     );
--- src/views/HomePage.tsx
+++ src/views/HomePage.tsx
@@ -1,5 +1,4 @@
 import * as React from "react";
-
 import { observer, inject } from "mobx-react";
 import { IAppModel } from "models/i_appmodel";
 import Combobox from "shared/Combobox";
@@ -9,7 +8,7 @@ import Combobox from "shared/Combobox";
 export default class Home 
   extends React.Component<{appModel?: IAppModel}> {
 
-  handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  handleFlavorTextChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     this.props.appModel.flavorInput = e.target.value;
   }
 
@@ -19,20 +18,20 @@ export default class Home
     return (
       <div className="home">
         <h2>Home Page</h2>
-        <h3>Mouse Position: 
-          {appModel.mousePosition.x}, 
-          {appModel.mousePosition.y}</h3>
+        Here are some examples of hooking page elements to a view model:
+        <hr style={{ margin: "15px 0" }} />
+
+        <h3>Dynamic pointer input:</h3> 
+        Mouse Position: {appModel.mousePosition.x}, {appModel.mousePosition.y}
 
         <hr style={{ margin: "15px 0" }} />
 
-        <h3>Text Input Length tracking</h3>
+        <h3>Text Input</h3>
         <input type="text" value={ appModel.flavorInput }
-          onChange={this.handleInputChange}  />
-        <b>Length</b>: {appModel.flavorInput.length}<br />
-        <button onClick={appModel.setUppercase} >Make Upper Case</button>
-        <br />
-        <button onClick={appModel.setLowercase} >Make Lower Case</button>
-        <br />
+          onChange={this.handleFlavorTextChange} />
+        (<b>Length</b>: {appModel.flavorInput.length})<br />
+        <button onClick={appModel.setUppercase} >Make Upper Case</button><br />
+        <button onClick={appModel.setLowercase} >Make Lower Case</button><br />
         <button onClick={appModel.addFlavor
            /* TODO: link the style to the app model so that this 
            button is disabled if there is no text in the input */} >Add</button>
@@ -47,7 +46,8 @@ export default class Home
               appModel.selectedItem = item;
             }}
           /><br />
-          <b>Selected Item</b>: { appModel.selectedItem }
+          <b>Selected Item</b>: { appModel.selectedItem }<br/>
+          <b>Item Count</b>: { appModel.flavors.length }<br/>
       </div>
     );
   }
