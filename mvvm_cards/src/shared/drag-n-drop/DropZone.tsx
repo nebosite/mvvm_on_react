d2b694fd332afcea75eed0fea623685bc76337cc
Sviat Wellnine <wellninedotnet@gmail.com>
2020-01-26 21:15:46 +0200

    - solved a problem of the non-resetting highlight on the dropzone leave

--- mvvm_cards/src/shared/drag-n-drop/DropZone.tsx
+++ mvvm_cards/src/shared/drag-n-drop/DropZone.tsx
@@ -6,6 +6,13 @@ import bus from "./services/bus";
 
 import { findDroppable, getElementUnderClientXY } from "shared/util"
 
+// TODO: Move it to shared if useful
+function isInDropZone(elementUnderMouse: any, dropZone: HTMLElement): any {
+  if (!elementUnderMouse) { return false };
+  const parentNode = elementUnderMouse.parentNode;
+  return elementUnderMouse === dropZone ? true : isInDropZone(parentNode, dropZone);
+}
+
 type Props = {
   children: React.ReactNode | React.ReactNode[];
   className?: string;
@@ -14,6 +21,9 @@ type Props = {
 export default class DropZone extends React.Component<Props> {
 
   rootElementRef: React.RefObject<HTMLDivElement> = React.createRef()
+
+
+  highlightedEl: any;
 
   onHover = () => {
     alert("On Hover")
@@ -25,16 +35,34 @@ export default class DropZone extends React.Component<Props> {
     const currentTargetElement = bus.data.avatar.getCurrentTargetElement()
     const prevTargetElement = bus.data.avatar.getPrevTargetElement()
 
+    console.log("rootEl", this.rootElementRef);
+
     currentTargetElement.classList.add("drag-zone-sector-over");
+    this.highlightedEl = currentTargetElement;
+
+    console.log("currentTargetElement => ", currentTargetElement)
+    
+
+    // console.log('1', isInDropZone(currentTargetElement, this.rootElementRef.current))
     // it could be null on the first dropzone-sector enter
     if (prevTargetElement) {
+      // console.log("prevTargetElement => ", prevTargetElement)
       prevTargetElement.classList.remove("drag-zone-sector-over");
+
+      
     }
    
 
 
 
     // console.log("Move ::::::::: e", bus.data.avatar.getTargetElement(), e, e.currentTarget, e.relativeTarget);
+  }
+
+  handleMouseLeave = () => {
+    if (!this.highlightedEl) return;
+
+    // remove highlight on the dropZone leave
+    this.highlightedEl.classList.remove("drag-zone-sector-over");
   }
 
   render() {
@@ -44,6 +72,7 @@ export default class DropZone extends React.Component<Props> {
     return (
       <div ref={this.rootElementRef} className={cn("drop-zone", className)} 
         onMouseMove={this.handleMouseMove}
+        onMouseLeave={this.handleMouseLeave}
         >
           <section className="drop-zone-sector" style={{ height: "100px", display: "block", backgroundColor: "orange" }}>
             A
